81a0a78cfc390e9eef5886fa2dd91a047eac22a6
sidewinder2020 <sdwinder1@gmail.com>
2017-08-07 14:17:15 -0600

    I extension: changes color upon guess

--- web_guesser.rb
+++ web_guesser.rb
@@ -7,7 +7,8 @@ set :number, rand(0..100)
 get '/' do
   guess = params["guess"].to_i
   guess_method = number_guesser(guess)
-  erb :index, :locals => {:number => settings.number, :guess_method => guess_method}
+  color = background_color_determiner(guess)
+  erb :index, :locals => {:number => settings.number, :guess_method => guess_method, :color => color}
 end
 
 def number_guesser(guess)
@@ -21,5 +22,15 @@ def number_guesser(guess)
     "Too low!"
   else
     "You got it right! The SECRET NUMBER is #{settings.number}"
+  end
+end
+
+def background_color_determiner(guess)
+  if guess < (settings.number - 5) || guess > (settings.number + 5)
+    "#ff0000"
+  elsif guess > settings.number || guess < settings.number
+    "#ff969d"
+  else
+    "#fcfcfc"
   end
 end
